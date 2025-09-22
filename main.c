37057367cee07f0ac15b14fc6a6395343d8c7c12
Estell Ulmer <estellul@te-g9.tash.21-school.uz>
2025-09-22 19:52:53 +0500

    switch case break

--- main.c
+++ main.c
@@ -3,29 +3,39 @@
 
 int main(){
 
-    int age = 0;
-    printf("yoshingizni kiriting: ");
-    scanf("%d", &age);
+    int day = 0;
+    printf("iltimos 1-7 gacha bolgan son tanlang: ");
+    scanf("%d", &day);
 
-    if (age > 50){
-        printf("Pensyaner\n");
-        bool tirik = false;
-        if (tirik)
-        {
-            printf("u hali ko'p yashaydi\n");
-        }else{
-            printf("afsus\n");
-        }
-        
-    }else if (age > 18){
-        printf("o'smir\n");
-    }else if (age > 1){
-        printf("chaqaloq\n");
-    }else {
-        printf("tug'ilmagan\n");
+    switch (day)
+    {
+    case 1:
+        printf("dushanba\n");
+        break;
+    case 2:
+        printf("seshanba\n");
+        break;
+    case 3:
+        printf("chorshanba\n");
+        break;
+    case 4:
+        printf("payshanba\n");
+        break;
+    case 5:
+        printf("juma\n");
+        break;
+    case 6:
+        printf("shanba\n");
+        break;
+    case 7:
+        printf("yakshanba\n");
+        break;
+    
+    default:
+        printf("iltimos 1-7 gacha bolgan sonni qayta tanlang!\n");
+        main();
+        break;
     }
-    
-    
 
     return 0;
 };      
